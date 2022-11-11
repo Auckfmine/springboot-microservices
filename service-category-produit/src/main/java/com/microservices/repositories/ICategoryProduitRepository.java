87303f08c8fc615f52daff4080b8ffbddefbb814
Auckfmine <mouhamedaminerouatbi@gmail.com>
2022-11-11 13:37:17 +0100

    prepare code for medium article

--- service-category-produit/src/main/java/com/microservices/repositories/ICategoryProduitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.repositories;
-
-import com.microservices.entities.CategoryProduit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICategoryProduitRepository extends JpaRepository<CategoryProduit,Long> {
-}
